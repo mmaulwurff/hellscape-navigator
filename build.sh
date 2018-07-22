1e1f863bc0ea67247085268b68f35eabf7aaf273
Alexander <mmaulwurff@gmail.com>
2018-07-22 20:13:51 +0700

    initial commit

---
+++ build.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+name=hellscape-navigator
+
+rm -f $name.pk3 \
+&& \
+git log --date=short --pretty=format:"-%d %ad %s%n" | \
+    grep -v "^$" | \
+    sed "s/HEAD -> master, //" | \
+    sed "s/, origin\/master//" | \
+    sed "s/ (HEAD -> master)//" | \
+    sed "s/ (origin\/master)//"  |\
+    sed "s/- (tag: \(v\?[0-9.]*\))/\n\1\n-/" \
+    > changelog.txt \
+&& \
+zip $name.pk3 \
+    *.txt \
+    *.md \
+&& \
+cp $name.pk3 $name-$(git describe --abbrev=0 --tags).pk3 \
+&& \
+gzdoom -glversion 3 \
+       \ #-iwad ~/Programs/Games/wads/doom/freedoom1.wad \
+       \ #-iwad ~/Programs/Games/wads/doom/freedoom2.wad \
+       -file \
+       $name.pk3 \
+       ~/Programs/Games/wads/maps/DOOMTEST.wad \
+       "$1" "$2" \
+       +map test
