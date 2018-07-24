4876ec29276f7beaf33892a74d72a4a4075adeee
Alexander <mmaulwurff@gmail.com>
2018-07-24 21:35:41 +0700

    add circle and arrow

--- make-sprites.sh
+++ make-sprites.sh
@@ -34,6 +34,22 @@ convert sprites/hnr1a0.png $params 292.5 sprites/hnrFa0.png
 convert sprites/hnr1a0.png $params 315   sprites/hnr8a0.png
 convert sprites/hnr1a0.png $params 337.5 sprites/hnrGa0.png
 
+convert sprites/hna1a0.png $params 22.5  sprites/hna9a0.png
+convert sprites/hna1a0.png $params 45    sprites/hna2a0.png
+convert sprites/hna1a0.png $params 67.5  sprites/hnaAa0.png
+convert sprites/hna1a0.png $params 90    sprites/hna3a0.png
+convert sprites/hna1a0.png $params 112.5 sprites/hnaBa0.png
+convert sprites/hna1a0.png $params 135   sprites/hna4a0.png
+convert sprites/hna1a0.png $params 157.5 sprites/hnaCa0.png
+convert sprites/hna1a0.png $params 180   sprites/hna5a0.png
+convert sprites/hna1a0.png $params 202.5 sprites/hnaDa0.png
+convert sprites/hna1a0.png $params 225   sprites/hna6a0.png
+convert sprites/hna1a0.png $params 247.5 sprites/hnaEa0.png
+convert sprites/hna1a0.png $params 270   sprites/hna7a0.png
+convert sprites/hna1a0.png $params 292.5 sprites/hnaFa0.png
+convert sprites/hna1a0.png $params 315   sprites/hna8a0.png
+convert sprites/hna1a0.png $params 337.5 sprites/hnaGa0.png
+
 optipng sprites/*
 
 wine ~/Programs/doomcrap-0.1.4/util/grabpng.exe -grab "w*0.5" "h*0.5" sprites/*
