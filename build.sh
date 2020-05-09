8ba8032a14ad4165dfb2ab05542e65c3f8d3b733
Alexander Kromm <mmaulwurff@gmail.com>
2020-05-09 13:42:28 +0700

    minor cleanup

--- build.sh
+++ build.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
+set -e
+
 #./make-sprites.sh
 
-name=hellscape-navigator
-lasttag=$(git describe --abbrev=0 --tags)
+name=hellscape-navigator-$(git describe --abbrev=0 --tags).pk3
 
-#IWAD="-iwad /home/allkromm/Programs/Games/wads/doom/freedoom1.wad"
+rm -f $name.pk3
 
-rm -f $name.pk3 \
-&& \
 git log --date=short --pretty=format:"-%d %ad %s%n" | \
     grep -v "^$" | \
     sed "s/HEAD -> master, //" | \
@@ -16,26 +15,14 @@ git log --date=short --pretty=format:"-%d %ad %s%n" | \
     sed "s/ (HEAD -> master)//" | \
     sed "s/ (origin\/master)//"  |\
     sed "s/- (tag: \(v\?[0-9.]*\))/\n\1\n-/" \
-    > changelog.txt \
-&& \
-sed -i 's/Version.*/Version '$lasttag'/' Readme.md \
-&& \
-zip $name.pk3 \
-    *.txt \
-    *.md  \
-    *.zs  \
-    sprites/*/*.png  \
-    sprites/*.png    \
-    graphics/*/*.png \
-    zscript/*.zs     \
-    zscript/*/*.zs   \
-    language.enu     \
-&& \
-cp $name.pk3 $name-$lasttag.pk3 \
-&& \
-gzdoom $IWAD \
-       -file \
-       $name.pk3 \
-       ~/Programs/Games/wads/maps/DOOMTEST.wad \
-       "$1" "$2" \
-       +map test -nomonsters +vid_fps true
+    > changelog.txt
+
+sed -i 's/Version.*/Version '$lasttag'/' Readme.md
+
+zip -R "$name" \
+    "*.md"  \
+    "*.png" \
+    "*.txt" \
+    "*.zs"
+
+gzdoom -file "$name.pk3" "$@"
